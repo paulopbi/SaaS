764aeb81f03e330520081e22cc61c3602cf098c3
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-18 19:55:35 -0300

    feat: add hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,56 @@
+import ArrowIcon from "@/assets/arrow-right.svg";
+import CogImage from "@/assets/cog.png";
+import cylinderImage from "@/assets/cylinder.png";
+import noodleImage from "@/assets/noodle.png";
+import Image from "next/image";
+
 export const Hero = () => {
-  return null;
+  return (
+    <section className="pt-8 pb-20 md:pt-5 mb:pb-10 bg-gradient-to-t from-[#183ec2] from-200% to-100% to-[#eaeefe]  overflow-x-clip">
+      <div className="container">
+        <div className="md:flex items-center">
+          <div className="md:w-[478px]">
+            <div className="font-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+              Version 2.0 is here
+            </div>
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-6">
+              Pathway to productivity
+            </h1>
+            <p className="text-xl text-[#010d3e] tracking-tight mt-6">
+              Celebrate the joy of accomplishment with an app designed to track
+              your progress, motivate your efforts, and celebrate your
+              successes.
+            </p>
+            <div className="flex gap-1 items-center mt-[30px]">
+              <button className="btn btn-primary">Get for free</button>
+              <button className="btn btn-text gap-1">
+                <span>Learn more</span>
+                <ArrowIcon className="h-5 w-5" />
+              </button>
+            </div>
+          </div>
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
+            <Image
+              src={CogImage}
+              alt="Cog Image"
+              className="md:absolute md:top-0 md:w-auto md:h-full md:max-w-none md:-left-6 lg:left-0"
+            />
+            <Image
+              src={cylinderImage}
+              width={220}
+              height={220}
+              alt="Cylinder Image"
+              className="hidden md:absolute -top-8 -left-32 "
+            />
+            <Image
+              src={noodleImage}
+              width={220}
+              alt="Noodle Image"
+              className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
