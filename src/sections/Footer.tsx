b53fbff41bc929cff0fa98a8ca2e176afa31de65
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-19 11:11:44 -0300

    feat: add footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,37 @@
+import logo from "@/assets/logosaas.png";
+import Image from "next/image";
+import SocialX from "@/assets/social-x.svg";
+import SocialInsta from "@/assets/social-insta.svg";
+import SocialLinkedin from "@/assets/social-linkedin.svg";
+import SocialPin from "@/assets/social-pin.svg";
+import SocialYoutube from "@/assets/social-youtube.svg";
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer className="bg-black text-[#bcbcbc] text-sm py-10 text-center">
+      <div className="container">
+        <div className="inline-flex relative before:content-[''] before:absolute before:top-2 before:bottom-0 before:bg-[linear-gradient(to_right,#f87bff,#fb92cf,#ffdd9b,#c2f081,#2fd8fe)] before:w-full before:blur">
+          <Image src={logo} alt="Logo image" height={40} className="relative" />
+        </div>
+        <nav className="flex flex-col md:flex-row md:justify-center gap-6 mt-6 ">
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Costumers </a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Careers</a>
+        </nav>
+        <div className="flex justify-center gap-6 mt-6">
+          <SocialX />
+          <SocialInsta />
+          <SocialLinkedin />
+          <SocialPin />
+          <SocialYoutube />
+        </div>
+        <p className="mt-6">
+          &copy; 2024 Paulo Victor, alguns direitos reservados.
+        </p>
+      </div>
+    </footer>
+  );
 };
