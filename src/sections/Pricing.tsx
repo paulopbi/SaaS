790de19395c7a0b0529c553d0fe5b61ddc1c3081
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-19 10:25:46 -0300

    chore: add tailwind custom utility card

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -55,7 +55,7 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <section className="py-24">
+    <section className="py-24 bg-white">
       <div className="container">
         <div className="section-heading">
           <h2 className="section-title">Pricing</h2>
@@ -78,7 +78,7 @@ export const Pricing = () => {
               <div
                 key={title}
                 className={twMerge(
-                  "p-10 rounded-3xl border border-[#f1f1f1] shadow-[0_7px_14px_#eaeaea] max-w-xs w-full",
+                  "card",
                   inverse === true && "border-black bg-black text-white"
                 )}
               >
